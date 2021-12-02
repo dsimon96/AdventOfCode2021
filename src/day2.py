2e69aec952763e8b1efda41071a0506575bfd1f0
David Simon <D@vidSimon.tech>
2021-12-02 06:21:24 +0000

    Reorganize directory structure, solve day 2

---
+++ src/day2.py
@@ -0,0 +1,89 @@
+import sys
+from enum import Enum, auto
+from dataclasses import dataclass, replace
+from typing import Callable, Generator, Iterable, TypeVar
+import click
+
+
+@click.group()
+def main():
+    pass
+
+
+class Direction(Enum):
+    FORWARD = auto()
+    DOWN = auto()
+    UP = auto()
+
+
+@dataclass
+class Command:
+    direction: Direction
+    units: int
+
+
+def get_input() -> Generator[Command, None, None]:
+    for line in sys.stdin.readlines():
+        dir_s, units_s = line.split()
+        yield Command(direction=Direction[dir_s.upper()], units=int(units_s))
+
+
+@dataclass
+class Position:
+    depth: int = 0
+    horizontal_pos: int = 0
+
+
+@dataclass
+class PositionAndAim:
+    position: Position = Position()
+    aim: int = 0
+
+
+def update_position(pos: Position, command: Command) -> Position:
+    match command.direction:
+        case Direction.FORWARD:
+            return replace(pos, horizontal_pos=pos.horizontal_pos+command.units)
+        case Direction.UP:
+            return replace(pos, depth=pos.depth-command.units)
+        case Direction.DOWN:
+            return replace(pos, depth=pos.depth+command.units)
+
+State = TypeVar('State')
+
+def do_command_sequence(
+    commands: Iterable[Command],
+    initial_state: State,
+    update_fn: Callable[[State, Command], State]
+) -> State:
+    state = initial_state
+    for command in commands:
+        state = update_fn(state, command)
+    return state
+
+@main.command()
+def part1():
+    pos = do_command_sequence(get_input(), Position(), update_position)
+    print(pos.depth * pos.horizontal_pos)
+
+def update_position_and_aim(pos_and_aim: PositionAndAim, command: Command) -> PositionAndAim:
+    match command.direction:
+        case Direction.FORWARD:
+            pos = pos_and_aim.position
+            new_pos = replace(pos,
+                horizontal_pos=pos.horizontal_pos+command.units,
+                depth=pos.depth+pos_and_aim.aim*command.units)
+            return replace(pos_and_aim, position=new_pos)
+        case Direction.UP:
+            return replace(pos_and_aim, aim=pos_and_aim.aim-command.units)
+        case Direction.DOWN:
+            return replace(pos_and_aim, aim=pos_and_aim.aim+command.units)
+
+@main.command()
+def part2():
+    pos_and_aim = do_command_sequence(get_input(), PositionAndAim(), update_position_and_aim)
+    print(pos_and_aim.position.depth * pos_and_aim.position.horizontal_pos)
+
+
+if __name__ == "__main__":
+    main()
